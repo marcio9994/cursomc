ff6b2dc018961690caa358ff47d7380f93fc6251
Marcio Sued <marciosuedlima@hotmail.com>
2019-03-21 18:19:39 -0300

    Cliente, TipoCiente, telefones e enderecos

--- src/main/java/com/marcio/cursomc/CursomcApplication.java
+++ src/main/java/com/marcio/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.marcio.cursomc.domain.Categoria;
 import com.marcio.cursomc.domain.Cidade;
+import com.marcio.cursomc.domain.Cliente;
+import com.marcio.cursomc.domain.Endereco;
 import com.marcio.cursomc.domain.Estado;
 import com.marcio.cursomc.domain.Produto;
+import com.marcio.cursomc.domain.enums.TipoCliente;
 import com.marcio.cursomc.repositories.CategoriaRepository;
 import com.marcio.cursomc.repositories.CidadeRepository;
+import com.marcio.cursomc.repositories.ClienteRepository;
+import com.marcio.cursomc.repositories.EnderecoRepository;
 import com.marcio.cursomc.repositories.EstadoRepository;
 import com.marcio.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -64,6 +73,17 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "333344", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("111111", "1111115"));
+		
+		Endereco e1 = new Endereco(null, "Rua flores", "300", "apt 103", "Jardim", "60410222", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida matos", "105", "Sala 800", "Centro", "604108777", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 	}
 
--- src/main/java/com/marcio/cursomc/domain/Categoria.java
+++ src/main/java/com/marcio/cursomc/domain/Categoria.java
@@ -14,8 +14,8 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Categoria implements Serializable {
-
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
